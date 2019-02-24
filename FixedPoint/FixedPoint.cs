65d1a66610b51bab9ee09a5370cbeb564ef423ee
andrewsil1 <andrewsi@users.noreply.github.com>
2019-02-23 22:41:16 -0800

    Dramatic simplification of conversion functions, and fixed bug that caused negative numbers with fractional component to convert incorrectly.

--- FixedPoint/FixedPoint.cs
+++ FixedPoint/FixedPoint.cs
@@ -14,14 +14,14 @@
         #region Properties
         private int Width { get; }
         public int FracBits { get; }
-        public ulong Value { get; }
+        public long Value { get; }
         public int IntBits => Width - FracBits;
         #endregion
 
         #region Constructors
         public FixedPoint(int width, int fracBits)
         {
-            if (width > sizeof(ulong) * 8)
+            if (width > sizeof(long) * 8)
             {
                 throw new ArgumentOutOfRangeException("width", "FixedPoint only supports up to a maximum of 64 bits in width.");
             }
@@ -43,11 +43,9 @@
 
         #region Private Methods
         /* Convert a double to a FixedPoint using a previously constructed template object of known radixes. */
-        private ulong Convert(double inVal)
+        private long Convert(double inVal)
         {
-            var temp = inVal * Math.Pow(2, this.FracBits);
-            temp = Math.Round(temp);
-            return (ulong)Math.Floor(temp);
+            return (long)Math.Round(inVal * (1L << FracBits));
         }
         #endregion
 
@@ -58,12 +56,27 @@
             Console.WriteLine("Converted to 16.16: {0:X16}", me.Convert(Math.PI));
             me = new FixedPoint(64, 32);
             Console.WriteLine("Converted to 32.32: {0:X16}", me.Convert(Math.PI));
-            me = new FixedPoint(40, 5);
+            me = new FixedPoint(40, 35);
             Console.WriteLine("Converted to 5.35: {0:X16}", me.Convert(Math.PI));
 
             //Test IValueConverter methods
-            me = new FixedPoint(40, 5, 2.25);
-            Console.WriteLine("2.25 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, 2.25);
+            Console.WriteLine("2.25 (5.35) reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, -2.0);
+            Console.WriteLine("-2.0 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, -2.5);
+            Console.WriteLine("-2.5 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, -3.0);
+            Console.WriteLine("-3.0 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, 3.5);
+            Console.WriteLine("3.5 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, -3.5);
+            Console.WriteLine("-3.5 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, 1);
+            Console.WriteLine("1 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            me = new FixedPoint(40, 35, -1);
+            Console.WriteLine("-1 reverse converted to string: {0}", me.Convert(me, typeof(FixedPoint), null, CultureInfo.CurrentCulture));
+            Console.ReadLine();
         }
 
         #region IValueConverter methods
@@ -72,38 +85,7 @@
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             FixedPoint input = (FixedPoint)value;
-            ulong sign = input.Value >> 63;                     // Determine sign bit
-            long intPart = (long)input.Value >> input.FracBits; // Shift right to lop off the fractional part.
-
-            string SignStr = string.Empty;
-            if (sign == 1)                                      // If negative convert to string representation of integer abs value.
-            {
-               intPart = Math.Abs(intPart);
-               SignStr = "-";
-            }
-            string stringInt = SignStr + intPart.ToString();
-
-            // Now create double representation of fractional part by evaluating each bit's contribution.
-            // Align all the fractional bits left to the MSB of the containing ulong.
-            ulong frac = input.Value << ((sizeof(ulong)*8) - input.FracBits);    
-            double fracDbl = 0;                                 
-            for (int x = -1; x >= -FracBits; x--)
-            {
-                if (frac >> 63 == 1)
-                {
-                    fracDbl += Math.Pow(2, x);                  // Raise all 1 bits to the 2^(-x) power to get its contribution to the fraction
-                }
-                frac = frac << 1;
-            }
-
-            if (fracDbl == 0)
-            {
-                return stringInt + ".0";
-            }
-            else
-            {
-                return stringInt + "." + fracDbl.ToString().Substring(2);
-            }
+            return (input.Value / (double) (1L << input.FracBits)).ToString("G17");
         }
 
         /* Converts string back into Fixed Point */
